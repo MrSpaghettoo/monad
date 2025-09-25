202ff21282bc0e556424091786bca82aa23a962f
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-09-25 16:47:13 -0400

    Separate fuzzer input generation from execution

--- category/vm/fuzzing/generator/generator.hpp
+++ category/vm/fuzzing/generator/generator.hpp
@@ -272,7 +272,8 @@ namespace monad::vm::fuzzing
         bool is_jump_dest;
     };
 
-    struct BasicBlock {
+    struct BasicBlock
+    {
         bool is_main;
         bool is_exit;
         bool is_jump_dest;
@@ -1107,13 +1108,16 @@ namespace monad::vm::fuzzing
         return contract;
     }
 
-    std::vector<std::uint8_t> compile_program(std::vector<BasicBlock> basic_blocks) {
+    std::vector<std::uint8_t>
+    compile_program(std::vector<BasicBlock> basic_blocks)
+    {
         auto prog = std::vector<std::uint8_t>{};
         auto jumpdest_patches = std::vector<std::pair<std::size_t, BlockIx>>{};
         auto block_offsets = std::vector<std::uint32_t>{};
 
         for (auto const &b : basic_blocks) {
-            compile_block(prog, b.instructions, jumpdest_patches, block_offsets);
+            compile_block(
+                prog, b.instructions, jumpdest_patches, block_offsets);
         }
         patch_jumpdests(prog, jumpdest_patches, block_offsets);
         return prog;
@@ -1124,8 +1128,8 @@ namespace monad::vm::fuzzing
         GeneratorFocus focus, Engine &eng, evmc_revision rev,
         std::vector<evmc::address> const &valid_addresses)
     {
-        auto basic_blocks = generate_basic_blocks(
-            focus, eng, rev, valid_addresses);
+        auto basic_blocks =
+            generate_basic_blocks(focus, eng, rev, valid_addresses);
         return compile_program(std::move(basic_blocks));
     }
 
@@ -1222,7 +1226,7 @@ namespace monad::vm::fuzzing
      * instantiating this lookup as appropriate.
      */
     template <typename Engine, typename LookupFunc>
-    message_ptr generate_message(
+    evmc_message generate_message(
         GeneratorFocus const &focus, Engine &eng,
         std::vector<evmc::address> const &contract_addresses,
         std::vector<evmc::address> const &known_eoas,
@@ -1282,7 +1286,7 @@ namespace monad::vm::fuzzing
 
         auto const &code = address_lookup(target);
 
-        return message_ptr{new evmc_message{
+        return evmc_message{
             .kind = kind,
             .flags = flags,
             .depth = depth,
@@ -1297,7 +1301,7 @@ namespace monad::vm::fuzzing
             .code_address = target,
             .code = code.data(),
             .code_size = code.size(),
-        }};
+        };
     }
 
 }
