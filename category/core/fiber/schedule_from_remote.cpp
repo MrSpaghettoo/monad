b80213efb50607ab2089f391e4b731064564d042
James Hunsaker <jhunsaker@users.noreply.github.com>
2025-10-15 13:38:20 -0400

    scratch

---
+++ category/core/fiber/schedule_from_remote.cpp
@@ -0,0 +1,69 @@
+#include <boost/fiber/scheduler.hpp>
+
+#include <boost/assert.hpp>
+
+#include <boost/fiber/context.hpp>
+
+#if defined(BOOST_FIBERS_NO_ATOMICS)
+    #error invalid boost config
+#endif
+
+#include <category/core/fiber/priority_algorithm.hpp>
+
+#include <quill/Quill.h>
+
+namespace boost
+{
+    namespace fibers
+    {
+        void scheduler::schedule(context *ctx) noexcept
+        {
+            // LOG_INFO("JAMES1");
+
+            BOOST_ASSERT(nullptr != ctx);
+            BOOST_ASSERT(!ctx->ready_is_linked());
+            BOOST_ASSERT(!ctx->remote_ready_is_linked());
+            BOOST_ASSERT(!ctx->terminated_is_linked());
+            // remove context ctx from sleep-queue
+            // (might happen if blocked in timed_mutex::try_lock_until())
+            if (ctx->sleep_is_linked()) {
+                // unlink it from sleep-queue
+                ctx->sleep_unlink();
+            }
+            // push new context to ready-queue
+            algo_->awakened(ctx);
+        }
+
+        void scheduler::schedule_from_remote(context *ctx) noexcept
+        {
+            // LOG_INFO("JAMES2");
+
+            BOOST_ASSERT(nullptr != ctx);
+            // another thread might signal the main-context of this thread
+            BOOST_ASSERT(!ctx->is_context(type::dispatcher_context));
+            BOOST_ASSERT(this == ctx->get_scheduler());
+            BOOST_ASSERT(!ctx->ready_is_linked());
+            BOOST_ASSERT(!ctx->remote_ready_is_linked());
+            BOOST_ASSERT(!ctx->terminated_is_linked());
+            // boost::fibers::algo::algorithm* const algo = algo_.get();
+            if (auto *const algo2 =
+                    dynamic_cast<monad::fiber::PriorityAlgorithm *>(
+                        algo_.get());
+                algo2) {
+                algo2->awakened(ctx);
+            }
+            else {
+                // protect for concurrent access
+                detail::spinlock_lock lk{remote_ready_splk_};
+                BOOST_ASSERT(!shutdown_);
+                BOOST_ASSERT(nullptr != main_ctx_);
+                BOOST_ASSERT(nullptr != dispatcher_ctx_.get());
+                // push new context to remote ready-queue
+                ctx->remote_ready_link(remote_ready_queue_);
+                lk.unlock();
+            }
+            // notify scheduler
+            algo_->notify();
+        }
+    }
+}
