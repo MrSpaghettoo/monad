16ea0d5d083be953ba362894f82e490c7d47da53
rtsaimonad <rtsai@category.xyz>
2025-11-18 11:58:32 -0800

    testing latest 0.12.2-rc on donet

--- category/execution/monad/chain/monad_devnet.cpp
+++ category/execution/monad/chain/monad_devnet.cpp
@@ -21,9 +21,12 @@
 
 MONAD_NAMESPACE_BEGIN
 
-monad_revision MonadDevnet::get_monad_revision(uint64_t /*timestamp*/) const
+monad_revision MonadDevnet::get_monad_revision(uint64_t timestamp) const
 {
-    return MONAD_NEXT;
+    if (MONAD_LIKELY(timestamp >= 1763507545)) {
+        return MONAD_EIGHT;
+    }
+    return MONAD_SEVEN;
 }
 
 uint256_t MonadDevnet::get_chain_id() const
